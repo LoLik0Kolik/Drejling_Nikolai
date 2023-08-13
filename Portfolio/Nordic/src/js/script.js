cf6d1d7bfd043c14943d5edb0ac4c10f4480106c
Nikolai <drejling_kolya_2@mail.ru>
2023-08-13 12:57:17 +0300

    end pulse

--- Portfolio/Nordic/src/js/script.js
+++ Portfolio/Nordic/src/js/script.js
@@ -5,4 +5,21 @@ $(document).ready(function(){
         nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/next.png"></button>'
         
     });
+    $('.slider_news').slick({
+        dots: false,
+        infinite: true,
+        speed: 200,
+        slidesToShow: 1,
+        centerMode: false,
+        variableWidth: true,
+        prevArrow:false,
+        nextArrow: false,
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+      });
+      $('.media_slider').slick({
+        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider_media/prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider_media/next.png"></button>'
+      });
   });
