a56312fb275b92b85c26f9a84e91b4ffcc8a7ca0
Nikolai <drejling_kolya_2@mail.ru>
2023-08-24 12:37:09 +0300

    Nordic end

--- Portfolio/Nordic/src/js/script.js
+++ Portfolio/Nordic/src/js/script.js
@@ -1,8 +1,8 @@
 $(document).ready(function(){
     $('.slider').slick({
 
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider/prev.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider/next.png"></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider/prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider/next.png"></button>'
         
     });
     $('.slider_news').slick({
@@ -19,7 +19,20 @@ $(document).ready(function(){
         autoplaySpeed: 2000,
       });
       $('.media_slider').slick({
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/slider_media/prev.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/slider_media/next.png"></button>'
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/slider_media/prev.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/slider_media/next.png"></button>'
       });
+      $('.exit_buffet_slider_wrapper').slick({
+        dots: false,
+        infinite: true,
+        speed: 200,
+        slidesToShow: 1,
+        centerMode: false,
+        variableWidth: true,
+        prevArrow:false,
+        nextArrow: false,
+        slidesToShow: 3,
+        autoplay: true,
+        autoplaySpeed: 2000
+      })
   });
